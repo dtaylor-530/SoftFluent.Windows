d869c7a592502d2e5e99adcc5a3cdc5f014c30cc
dtaylor-530 <d.taylor530@gmail.com>
2023-03-01 08:04:30 +0000

    Updates code with new Engine type and new datatemplates

---
+++ SoftFluent.Windows/PropertyGrid.WPF.Demo/Infrastructure/BaseActivator.cs
@@ -0,0 +1,20 @@
+﻿using SoftFluent.Windows.Utilities;
+using System;
+using PropertyGrid.Infrastructure;
+
+namespace SoftFluent.Windows
+{
+    public class BaseActivator : IActivator
+    {
+        public virtual object? CreateInstance(Type type, params object?[] args)
+        {
+            if (type == null)
+            {
+                throw new ArgumentNullException("type");
+            }
+
+
+            return Activator.CreateInstance(type, args);
+        }
+    }
+}
---
+++ SoftFluent.Windows/PropertyGrid.WPF.Demo/Infrastructure/Engine.cs
@@ -0,0 +1,27 @@
+﻿using PropertyGrid.Abstractions;
+using SoftFluent.Windows;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertyGrid.WPF.Demo.Infrastructure
+{
+    public class Engine : IPropertyGridEngine
+    {
+        private readonly IActivator activator;
+
+        public Engine(IActivator activator)
+        {
+            this.activator = activator;
+        }
+
+        public IPropertyEngine Convert(IPropertyGridOptions options)
+        {
+            return new PropertyGridListSource(activator, options) { };
+        }
+
+        public static Engine Instance { get; } = new Engine(new BaseActivator());
+    }
+}
