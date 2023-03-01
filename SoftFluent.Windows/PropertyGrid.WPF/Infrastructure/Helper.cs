b8eed92216fe2fd558b00dbba0781ddc0d742cfd
dtaylor-530 <d.taylor530@gmail.com>
2023-03-01 21:02:40 +0000

    Makes SelectedObjectPropertyChanged async

--- SoftFluent.Windows/PropertyGrid.WPF/Infrastructure/Helper.cs
+++ SoftFluent.Windows/PropertyGrid.WPF/Infrastructure/Helper.cs
@@ -3,70 +3,84 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using Utilities;
 
-namespace SoftFluent.Windows {
+namespace SoftFluent.Windows
+{
 
-   public static class Helper {
+    public static class Helper
+    {
 
-      public static DataGridColumn DataGridColumn(DataGrid grid) {
-         return grid
-             .Columns
-             .OfType<DataGridTemplateColumn>()
-             .FirstOrDefault(c => c.CellTemplateSelector is PropertyGridDataTemplateSelector);
-      }
+        public static DataGridColumn DataGridColumn(DataGrid grid)
+        {
+            return grid
+                .Columns
+                .OfType<DataGridTemplateColumn>()
+                .FirstOrDefault(c => c.CellTemplateSelector is PropertyGridDataTemplateSelector);
+        }
 
 
-      public static HashSet<Type> GetTypes() {
-         return new HashSet<Type>(new[]
-         {
+        public static HashSet<Type> GetTypes()
+        {
+            return new HashSet<Type>(new[]
+            {
                 typeof(string), typeof(decimal), typeof(byte), typeof(sbyte), typeof(float), typeof(double),
                 typeof(int), typeof(uint), typeof(short), typeof(ushort), typeof(long), typeof(ulong),
                 typeof(bool), typeof(Guid), typeof(char),
                 typeof(Uri), typeof(Version)
                 // NOTE: timespan, datetime?
             });
-      }
+        }
 
-      public static bool HasProperties(Type type) {
-         if (type == null) {
-            throw new ArgumentNullException("type");
-         }
-
-         foreach (PropertyDescriptor descriptor in TypeDescriptor.GetProperties(type)) {
-            if (!descriptor.IsBrowsable) {
-               continue;
+        public static bool HasProperties(Type type)
+        {
+            if (type == null)
+            {
+                throw new ArgumentNullException("type");
             }
 
-            return true;
-         }
+            foreach (PropertyDescriptor descriptor in TypeDescriptor.GetProperties(type))
+            {
+                if (!descriptor.IsBrowsable)
+                {
+                    continue;
+                }
 
-         return false;
-      }
+                return true;
+            }
 
-      public static void IsReadOnlyPropertyChanged(DependencyObject source, DependencyPropertyChangedEventArgs e) {
-         PropertyGrid grid = (PropertyGrid)source;
-         grid.PropertiesSource.Source = grid.PropertiesSource.Source;
-      }
+            return false;
+        }
 
-      public static void GroupByCategoryChanged(DependencyObject source, DependencyPropertyChangedEventArgs e) {
-         PropertyGrid grid = (PropertyGrid)source;
-         grid.IsGrouping = (bool)e.NewValue;
-      }
+        public static void IsReadOnlyPropertyChanged(DependencyObject source, DependencyPropertyChangedEventArgs e)
+        {
+            PropertyGrid grid = (PropertyGrid)source;
+            grid.PropertiesSource.Source = grid.PropertiesSource.Source;
+        }
+
+        public static void GroupByCategoryChanged(DependencyObject source, DependencyPropertyChangedEventArgs e)
+        {
+            PropertyGrid grid = (PropertyGrid)source;
+            grid.IsGrouping = (bool)e.NewValue;
+        }
 
 
 
-      public static void OnEditorSelectorSelectionChanged(string childPropertyGridName, object sender, SelectionChangedEventArgs e) {
-         OnEditorSelectorSelectionChanged(null, childPropertyGridName, sender, e);
-      }
+        public static void OnEditorSelectorSelectionChanged(string childPropertyGridName, object sender, SelectionChangedEventArgs e)
+        {
+            OnEditorSelectorSelectionChanged(null, childPropertyGridName, sender, e);
+        }
 
-      public static void OnEditorSelectorSelectionChanged(PropertyGrid parentGrid, string childPropertyGridName, object sender, SelectionChangedEventArgs e) {
-         if (childPropertyGridName == null) {
-            throw new ArgumentNullException("childPropertyGridName");
-         }
+        public static void OnEditorSelectorSelectionChanged(PropertyGrid parentGrid, string childPropertyGridName, object sender, SelectionChangedEventArgs e)
+        {
+            if (childPropertyGridName == null)
+            {
+                throw new ArgumentNullException("childPropertyGridName");
+            }
 
             if (sender is FrameworkElement element && e.AddedItems.Count > 0)
             {
@@ -84,21 +98,24 @@ namespace SoftFluent.Windows {
             }
         }
 
-      public static void RefreshSelectedObject(DependencyObject editor) {
+        public static void RefreshSelectedObject(DependencyObject editor)
+        {
             foreach (PropertyGrid grid in editor.GetChildren<PropertyGrid>())
             {
                 grid.RefreshSelectedObject();
             }
         }
 
-      public static void SelectedObjectPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e) {
-         if (d is PropertyGrid propertyGrid) {
-            Helper.SelectedObjectPropertyChanged(propertyGrid, e);
-         }
-      }
+        public static void SelectedObjectPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is PropertyGrid propertyGrid)
+            {
+                Helper.SelectedObjectPropertyChanged(propertyGrid, e);
+            }
+        }
 
 
-        public static void SelectedObjectPropertyChanged(PropertyGrid grid, DependencyPropertyChangedEventArgs e)
+        public static async void SelectedObjectPropertyChanged(PropertyGrid grid, DependencyPropertyChangedEventArgs e)
         {
             if (e.OldValue is INotifyPropertyChanged pc)
             {
@@ -129,7 +146,7 @@ namespace SoftFluent.Windows {
             }
             if (grid.Engine == null)
                 return;
-            grid.PropertiesSource.Source = grid.Engine.Convert(grid.Options);
+            grid.PropertiesSource.Source = await Task.Run(() => grid.Engine.Convert(grid.Options));
 
             void OnDispatcherSourcePropertyChanged(object sender, PropertyChangedEventArgs eventArgs)
             {
@@ -146,65 +163,81 @@ namespace SoftFluent.Windows {
                 }
             }
         }
-        public static void SetGroupByCategory(PropertyGrid grid, bool value) {
-         switch (value) {
-            case true when !grid.GroupByCategory:
-               grid.PropertiesSource.GroupDescriptions.Add(new PropertyGroupDescription("Category"));
-               break;
+        public static void SetGroupByCategory(PropertyGrid grid, bool value)
+        {
+            switch (value)
+            {
+                case true when !grid.GroupByCategory:
+                    grid.PropertiesSource.GroupDescriptions.Add(new PropertyGroupDescription("Category"));
+                    break;
 
-            case false when grid.GroupByCategory:
-               grid.PropertiesSource.GroupDescriptions.Clear();
-               break;
-         }
-      }
-
-  
-
-      public static void UpdateBindings(PropertyGrid grid, object dataItem, string childName, Func<Binding, bool> where, Action<BindingExpression> action) {
-         if (dataItem == null) {
-            throw new ArgumentNullException("dataItem");
-         }
-
-         if (action == null) {
-            throw new ArgumentNullException("action");
-         }
-
-         if (!(grid.GetValueCellContent(dataItem) is var fe)) {
-            return;
-         }
-
-         if (childName == null) {
-            foreach (UIElement child in fe.EnumerateVisualChildren(true).OfType<UIElement>()) {
-               Action(child);
+                case false when grid.GroupByCategory:
+                    grid.PropertiesSource.GroupDescriptions.Clear();
+                    break;
             }
-         }
-         else {
-            FrameworkElement child = fe.FindVisualChild<FrameworkElement>(childName);
-            if (child != null) {
-               Action(child);
-            }
-         }
+        }
 
-         void Action(UIElement element) {
-            if (element == null) {
-               throw new ArgumentNullException("element");
+
+
+        public static void UpdateBindings(PropertyGrid grid, object dataItem, string childName, Func<Binding, bool> where, Action<BindingExpression> action)
+        {
+            if (dataItem == null)
+            {
+                throw new ArgumentNullException("dataItem");
             }
 
-            if (action == null) {
-               throw new ArgumentNullException("action");
+            if (action == null)
+            {
+                throw new ArgumentNullException("action");
             }
 
-            if (@where == null) {
-               @where = b => true;
+            if (!(grid.GetValueCellContent(dataItem) is var fe))
+            {
+                return;
             }
 
-            foreach (DependencyProperty prop in VisualTreeExtensions.EnumerateMarkupDependencyProperties(element)) {
-               BindingExpression expr = BindingOperations.GetBindingExpression(element, prop);
-               if (expr != null && expr.ParentBinding != null && @where(expr.ParentBinding)) {
-                  action(expr);
-               }
+            if (childName == null)
+            {
+                foreach (UIElement child in fe.EnumerateVisualChildren(true).OfType<UIElement>())
+                {
+                    Action(child);
+                }
             }
-         }
-      }
-   }
+            else
+            {
+                FrameworkElement child = fe.FindVisualChild<FrameworkElement>(childName);
+                if (child != null)
+                {
+                    Action(child);
+                }
+            }
+
+            void Action(UIElement element)
+            {
+                if (element == null)
+                {
+                    throw new ArgumentNullException("element");
+                }
+
+                if (action == null)
+                {
+                    throw new ArgumentNullException("action");
+                }
+
+                if (@where == null)
+                {
+                    @where = b => true;
+                }
+
+                foreach (DependencyProperty prop in VisualTreeExtensions.EnumerateMarkupDependencyProperties(element))
+                {
+                    BindingExpression expr = BindingOperations.GetBindingExpression(element, prop);
+                    if (expr != null && expr.ParentBinding != null && @where(expr.ParentBinding))
+                    {
+                        action(expr);
+                    }
+                }
+            }
+        }
+    }
 }
