b8d565c9ab9d3582ec18cf24b6059ff328610b67
dtaylor-530 <d.taylor530@gmail.com>
2023-03-03 20:17:40 +0000

    Removes the MainWindow

--- SoftFluent.Windows/PropertyGrid.WPF.Demo/App.xaml.cs
+++ SoftFluent.Windows/PropertyGrid.WPF.Demo/App.xaml.cs
@@ -8,6 +8,9 @@ namespace SoftFluent.Windows.Samples
     {
         protected override void OnStartup(StartupEventArgs e)
         {
+            var window = new Window { Content = new Customer2() };
+            window.Show();
+
             base.OnStartup(e);
 #if DEBUG
             Tracing.Enable();
